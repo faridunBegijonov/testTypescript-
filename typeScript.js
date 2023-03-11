4fd5dfb3458a5ee67e526b4cd8445b8121d3f01c
faridunBegijonov <faridunBegijonov010@gmail.com>
2023-03-11 14:37:54 +0500

    push array

---
+++ typeScript.js
@@ -0,0 +1,15 @@
+var num = 1000;
+var bonus = 500;
+var res = num + bonus;
+console.log(res);
+var user = {
+    name: 'Faridun',
+    age: 16
+};
+var sayHello = function (entety) {
+    return "".concat(entety.age, " hello ");
+};
+var users = [{ name: 'Faridun', age: 16 }, { name: 'Farukh', age: 26 }];
+var car = ['Hello', 10];
+car.push(20);
+console.log(car);
--- typeScript.ts
+++ typeScript.ts
@@ -16,3 +16,8 @@ const user: IUser = {
 const sayHello = (entety: IUser) => {
     return `${entety.age} hello `
 }
+
+const users: IUser[] = [{name: 'Faridun', age: 16}, {name: 'Farukh', age: 26}];
+const car: [string, number] = ['Hello', 10];
+car.push(20);
+console.log(car)
